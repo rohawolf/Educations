1b7793637adb82f71bfc9f88bf50cd782df86260
rohawolf <rohawolf@gmail.com>
2018-05-04 17:46:38 +0900

    update	20180504	17:46

--- mypractice/src/com/roha/user/dao/UserDao.java
+++ mypractice/src/com/roha/user/dao/UserDao.java
@@ -51,6 +51,23 @@ public class UserDao {
 		c.close();
 		
 		return user;
-	}	
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+ 		UserDao dao = new UserDao();
+ 		
+ 		User user = new User();
+ 		user.setId("rohawolf");
+ 		user.setName("Roha Park");
+ 		user.setPassword("roharoha");
+ 		
+ 		dao.add(user);
+		System.out.println(user.getId() + " add success");
+ 		
+ 		User user2 = dao.get(user.getId());
+ 		System.out.println(user2.getName());
+ 		System.out.println(user2.getPassword());
+		System.out.println(user2.getId() + " select success");
+ 	}
 }
 

f9ba7e720f37a32932cfba9dfb70320df1dd1a5c
rohawolf <rohawolf@gmail.com>
2018-05-04 15:36:06 +0900

    Update	20180504	15:36

--- member_proj/src/com/javateam/member/dao/MemberDAOImpl.java
+++ member_proj/src/com/javateam/member/dao/MemberDAOImpl.java
@@ -4,8 +4,10 @@
 package com.javateam.member.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.javateam.member.vo.MemberVO;
@@ -39,29 +41,31 @@ public final class MemberDAOImpl implements MemberDAO {
 		public static MemberDAOImpl getInstance() {
 			return Singleton.instance;
 		}
-	}
-	
+	}	
 	
 	/* (non-Javadoc)
 	 * @see com.javateam.member.dao.MemberDAO#connect()
 	 */
 	@Override
 	public Connection connect() { //throws Exception {
-		
-		//Driver properties
+		// JDBC direct connecting
+		//  Driver properties for Connection instance
 		final String driver = "oracle.jdbc.OracleDriver";			//JDBC Driver for Oracle
 		final String url = "jdbc:oracle:thin:@localhost:1521:xe";	//Connection URL
-		final String userid = "oraclejava";
-		final String userpw = "oraclejava";
+		final String user = "oraclejava";
+		final String password = "oraclejava";
 				
 		Connection con = null;	//initializing
 		
-		try {
+		try {			
 			Class.forName(driver);	//search for JDBC Driver for Oracle
-		} catch (ClassNotFoundException e) {
+			con = DriverManager.getConnection(url, user, password);			
+		} 
+		catch (ClassNotFoundException | SQLException e) {
+			// NOTICE! : JDK 1.7 over only
+			System.out.println("Error in connect() : ");
 			e.printStackTrace();
 		}		
-		
 		return con;
 	}
 
@@ -69,18 +73,61 @@ public final class MemberDAOImpl implements MemberDAO {
 	 * @see com.javateam.member.dao.MemberDAO#close(java.sql.ResultSet, java.sql.PreparedStatement, java.sql.Connection)
 	 */
 	@Override
-	public void close(ResultSet rs, PreparedStatement pstmt, Connection con) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void close(ResultSet rs, 
+					  PreparedStatement pstmt, 
+					  Connection con) { //throws Exception {		
+			try {				
+				if(rs != null)		rs.close();
+				if(pstmt != null)	pstmt.close();
+				if(con != null)		con.close();
+			} catch (SQLException e) {
+				System.out.println("Error in close() : ");
+				e.printStackTrace();
+			}
 	}
 
-	/* (non-Javadoc)
+	/**
 	 * @see com.javateam.member.dao.MemberDAO#insertMember(com.javateam.member.vo.MemberVO)
 	 */
 	@Override
-	public void insertMember(MemberVO member) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void insertMember(MemberVO member) { //throws Exception {
+			// 1. connect DB
+		Connection con = this.connect();
+		
+			// 2. construct SQL query
+		String sql = "INSERT INTO member "
+				+ 	 "VALUES(?, ?, ?, ?, SYSDATE)";
+				
+			// 3. make instance executing SQL query
+		PreparedStatement pstmt = null;		
+		try {
+			// 4. SQL query parsing
+			pstmt = con.prepareStatement(sql);
+			
+			// 5. set parameters of SQL query
+			pstmt.setString(1, member.getId());
+			pstmt.setString(2, member.getPw());
+			pstmt.setString(3, member.getName());
+			pstmt.setString(4, member.getAddress());
+			
+			// 6. execute SQL query, messaging
+			// int executeUpdate() : return the row count which had been updated.
+			//					 	 In this time, it should be 1.
+			if (pstmt.executeUpdate() == 1) {
+				// update success. (1 row updated.)
+				System.out.println("Thank you for join us, " + member.getId() + "!");
+			} else {
+				// no row updated.
+				System.out.println("fail to join.");
+			}
+			
+		} catch (SQLException e) {			
+			System.out.println("Error in insertMember() : ");
+			e.printStackTrace();			
+		} finally {
+			// 7. close all resources
+			this.close(null, pstmt, con);
+		}			
 	}
 
 	/* (non-Javadoc)

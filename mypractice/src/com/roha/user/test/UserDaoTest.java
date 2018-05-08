93b7317747fb86c32dff4e9a675e8491fd0f5634
rohawolf <rohawolf@gmail.com>
2018-05-08 17:06:30 +0900

    Update 20180508 17:05 Factory method pattern VS Application Context

--- mypractice/src/com/roha/user/test/UserDaoTest.java
+++ mypractice/src/com/roha/user/test/UserDaoTest.java
@@ -24,9 +24,12 @@ public class UserDaoTest {
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {	
 		
+		
+		DaoFactory factory = new DaoFactory();
+		
 		ApplicationContext context = 
 				new AnnotationConfigApplicationContext(DaoFactory.class);
-		
+		/*
  		UserDao dao = context.getBean("userDao", UserDao.class);
 		
  		User user = new User();
@@ -41,6 +44,25 @@ public class UserDaoTest {
  		System.out.println(user2.getName());
  		System.out.println(user2.getPassword());
 		System.out.println(user2.getId() + " select success");
+		*/
+		
+		UserDao dao1 = factory.userDao();
+		UserDao dao2 = factory.userDao();
+		
+		UserDao dao3 = context.getBean("userDao", UserDao.class);
+		UserDao dao4 = context.getBean("userDao", UserDao.class);
+		
+		// 매 호출마다  새로운 UserDao 객체 생성 후 리턴
+		System.out.println(dao1);
+		System.out.println(dao2);
+		System.out.println(dao1 == dao2); // => false
+		
+		// 첫 생성 이후 호출 시 기존에 생성되어 있던 객체 리턴
+		// ApplicationContext => singleton registry
+		System.out.println(dao3);
+		System.out.println(dao4);
+		System.out.println(dao3 == dao4); // => true
+		
  	}
 
 }

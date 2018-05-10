e0bd0f90743d59fb418bb53255abbde1cd021ea0
rohawolf <rohawolf@gmail.com>
2018-05-10 14:19:34 +0900

    Update	20180510 14:19

---
+++ member_proj/src/com/javateam/member/util/EncodingFilter.java
@@ -0,0 +1,61 @@
+package com.javateam.member.util;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		// request part
+		request.setCharacterEncoding("utf-8");
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		
+		if (session.getAttribute("ID_SESS") != null) {
+			// if "ID_SESS" exists
+		} else {
+			// not
+		}
+		chain.doFilter(request, response);
+		
+		//response part
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
